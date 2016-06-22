2a908c8d96c352313925b70b31aff423ece2d637
lingtran <lxiutran@gmail.com>
2016-06-21 18:14:02 -0600

    pass sort me exercise

---
+++ sort-by.js
@@ -0,0 +1,13 @@
+var _ = require('lodash');
+
+var items = [{ article: 41, quantity: 24 },
+             { article: 2323, quantity: 2},
+             { article: 655, quantity: 23}];
+
+var sortBy = function (items) {
+  return _.sortBy(items, function(item){
+    return item.quantity;
+  }).reverse();
+};
+
+module.exports = sortBy;
