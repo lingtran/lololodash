aba4f654982065c5a0b25b994ac974c70a1558c7
lingtran <lxiutran@gmail.com>
2016-06-21 19:51:59 -0600

    pass in every case exercise

--- in-every-case.js
+++ in-every-case.js
@@ -1,25 +1,13 @@
-var _ = require('lodash');
+var _ = require("lodash");
 
-var towns = {
-  Hamburg: { population: 1.698 },
-  Strasbourg: { population: 0.272 },
-  Rome: { population: 2.753 },
-  Dublin: { population: 0.528 }
-};
-
-var addSize = function(towns){
-  return _.forEach(
-    towns,
-    function(town){
-      if (town.population > 1.58){
-        town.size = 'big';
-      } else if(town.population < 0.28){
-        town.size:'small';
+module.exports = function(towns){
+  return _.forEach(towns, function(town){
+      if (town.population > 1) {
+        town.size = "big";
+      } else if (town.population > 0.5) {
+        town.size = "med";
       } else {
-        town.size: 'med';
+        town.size = "small";
       }
-  }
-);
+  });
 };
-
-module.exports = addSize;
